4199368dfefaba5485b6b2355f1239a39f65815c
velusip <velusip@gmail.com>
2013-09-21 08:30:29 -0400

    cleaned up formatting, changed key definition method

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,6 @@
 import XMonad
 import Control.Monad
-import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicLog -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-DynamicLog.html
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ICCCMFocus
@@ -9,66 +9,81 @@ import XMonad.Layout.NoBorders(noBorders, smartBorders)
 import XMonad.Layout.Spacing
 import XMonad.Util.WorkspaceCompare
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeysP)
+import Graphics.X11.ExtraTypes.XF86 -- http://xmonad.org/xmonad-docs/X11/Graphics-X11-ExtraTypes-XF86.html
 import System.IO
-
+import qualified Data.Map as M
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/velusip/.xmobarrc"
-    xmonad $ defaultConfig
-        { terminal = "urxvt"
-        , borderWidth = 4
-        , normalBorderColor = "#000000"
-        , focusedBorderColor = "#ffa90a" -- orange
-        , modMask = mod4Mask             -- Finland key
-        , focusFollowsMouse = True
-        , manageHook = composeAll
-            [ className =? "Gimp" --> doFloat
-            , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat
-            , manageDocks
-            ]
-        , logHook = takeTopFocus >> dynamicLogWithPP defaultPP
-            { ppCurrent         = xmobarColor "#545" "#c7e0e5"
-            , ppVisible         = wrap "(" ")"
-            , ppHidden          = id
-            , ppHiddenNoWindows = const ""
-            , ppUrgent          = xmobarColor "red" "black"
-            , ppSep             = " ★ "
-            , ppWsSep           = ""
-            , ppTitle           = xmobarRainbow . shorten 64
-            , ppLayout          = const "" -- suppress layout info
-            , ppOrder           = id
-            , ppOutput          = hPutStrLn xmproc
-            , ppSort            = getSortByIndex
-            , ppExtras          = []
-            }
-        , startupHook = ewmhDesktopsStartup
-                        >> setWMName "LG3D"
---                        >> takeTopFocus
-        , layoutHook = avoidStruts(tiled ||| Mirror tiled ||| noBorders Full)
-        } `additionalKeysP`
-            [ ("M-S-x", spawn "slock")
-            , ("M-b", sendMessage ToggleStruts)
-            , ("M-p", spawn "dmenu_run -i -fn '-*-gohufont-bold-*-*-*-14-*-*-*-*-*-*-*' -nb '#fff' -nf '#555' -sb '#ec826a' -sf '#555'")
-            , ("<XF86AudioNext>", spawn "cmus-remote --next")
-            , ("<XF86AudioPlay>", spawn "cmus-remote --pause")
-            , ("<XF86AudioPrev>", spawn "cmus-remote --prev")
-            , ("<XF86AudioStop>", spawn "cmus-remote --stop")
-            , ("<XF86AudioRaiseVolume>", spawn "cmus-remote --volume +10%")
-            , ("<XF86AudioLowerVolume>", spawn "cmus-remote --volume -10%")
-            , ("<XF86Reload>", spawn "cmus-remote --shuffle")
-            , ("<XF86Forward>", spawn "cmus-remote --seek +5")
-            , ("<XF86Back>", spawn "cmus-remote --seek -5")
-            , ("<Print>", spawn "/home/velusip/bin/ssur")
-            --, ("xK_Print", spawn "/home/velusip/bin/ssur")
-            , ("M-<Print>", spawn "/home/velusip/bin/ssur -u")
-            ]
-    where tiled = smartBorders $ smartSpacing 8 $ Tall 1 0.03 0.5
+    xmonad $ myConfig xmproc
 
--- | xmobar string colourising instructions.
+myConfig xmprocHandle = defaultConfig
+    { startupHook = myStartupHook
+    , layoutHook = myLayoutHook
+    , manageHook = myManageHook
+    , logHook = myLogHook xmprocHandle
+    , modMask = mod4Mask -- Finland key
+    , keys = myKeys <+> keys defaultConfig
+    , terminal = "urxvt"
+    , borderWidth = 4
+    , normalBorderColor = "#000000"
+    , focusedBorderColor = "#ffa90a" -- orange
+    , focusFollowsMouse = True
+    }
+
+myStartupHook = ewmhDesktopsStartup >> setWMName "LG3D"
+
+myLayoutHook = avoidStruts(tiled ||| Mirror tiled ||| noBorders Full)
+
+tiled = smartBorders $ smartSpacing 8 $ Tall 1 0.03 0.5
+
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat
+    , className =? "Steam" --> doFloat
+    , manageDocks
+    ]
+
+myLogHook :: Handle -> X ()
+myLogHook xmproc = takeTopFocus >> dynamicLogWithPP defaultPP
+    { ppCurrent         = xmobarColor "#545" "#c7e0e5"
+    , ppVisible         = wrap "(" ")"
+    , ppHidden          = id
+    , ppHiddenNoWindows = const ""
+    , ppUrgent          = xmobarColor "red" "black"
+    , ppSep             = " ★ "
+    , ppWsSep           = ""
+    , ppTitle           = xmobarRainbow . shorten 64
+    , ppLayout          = const "" -- suppress layout info
+    , ppOrder           = id
+    , ppOutput          = hPutStrLn xmproc
+    , ppSort            = getSortByIndex
+    , ppExtras          = []
+    }
+
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask .|. shiftMask, xK_x), spawn "slock")
+    , ((modMask, xK_b), sendMessage ToggleStruts)
+    , ((modMask, xK_p), spawn "dmenu_run -i -fn '-*-gohufont-bold-*-*-*-14-*-*-*-*-*-*-*' -nb '#fff' -nf '#555' -sb '#ec826a' -sf '#555'")
+    , ((modMask, xK_Print), spawn "ssur -u")
+    , ((0, xK_Print), spawn "ssur")
+    , ((0, xF86XK_AudioNext), spawn "cmus-remote --next")
+    , ((0, xF86XK_AudioPlay), spawn "cmus-remote --pause")
+    , ((0, xF86XK_AudioPrev), spawn "cmus-remote --prev")
+    , ((0, xF86XK_AudioStop), spawn "cmus-remote --stop")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "cmus-remote --volume +10%")
+    , ((0, xF86XK_AudioLowerVolume), spawn "cmus-remote --volume -10%")
+    , ((0, xF86XK_Reload), spawn "cmus-remote --shuffle")
+    , ((0, xF86XK_Forward), spawn "cmus-remote --seek +5")
+    , ((0, xF86XK_Back), spawn "cmus-remote --seek -5")
+    ]
+
+-- | for colourizing xmobar ppTitle
 xmobarRainbow :: String -> String
 xmobarRainbow = rainbow $ flip xmobarColor ""
--- | Rainbow-colours function.
+
+-- | Hardcoded rainbow colours
 -- first argument: a colouring function
 -- function takes a colour descriptor string and a string to colourise.
 rainbow :: (String -> String -> String) -> String -> String
